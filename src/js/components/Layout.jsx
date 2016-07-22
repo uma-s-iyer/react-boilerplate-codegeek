1ceef06ff92e4537d63547e92434df4c67e0ff76
Uma S Iyer <uma.iyer@mindtree.com>
2016-07-22 23:12:12 +0530

    Create Layout.jsx

---
+++ src/js/components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Layout extends React.Component {
+
+  render() {
+    return (
+      <div>
+        'Hello from React!'
+      </div>
+    );
+  }
+
+}
