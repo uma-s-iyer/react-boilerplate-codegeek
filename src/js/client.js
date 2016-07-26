ea26d320d04863189bda4f535797f8a2944c0faa
Uma S Iyer <uma.iyer@mindtree.com>
2016-07-26 10:05:02 +0530

    Update client.js

--- src/js/client.js
+++ src/js/client.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Layout from './components/Layout';
+import Layout from './components/Layout.jsx';
 
 const app = document.getElementById( 'app' );
 
